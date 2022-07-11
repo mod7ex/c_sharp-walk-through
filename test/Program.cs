5b062984830928c973b4d0f09daaa6aea0b333c3
Mourad <mouradface1998@gmail.com>
2022-07-11 21:57:56 +0100

    learning c#...

--- test/Program.cs
+++ test/Program.cs
@@ -78,8 +78,6 @@
             Console.WriteLine("String to single character; " + Convert.ToChar(someStr));
             Console.WriteLine("String to boolean; " + Convert.ToBoolean("false")); // only "true" & "false" can be converted to boolean
 
-            */
-
             // ---------------------------------- User input ----------------------------------
 
             Console.WriteLine("What is your name ? ");
@@ -94,7 +92,126 @@
             Console.Write("Hello " + user_name);
             Console.WriteLine(", you're " + user_age);
 
-            // Console.ReadKey();
+            // ---------------------------------- Math stuff ----------------------------------
+
+            Console.WriteLine(Math.Pow(4, 2)); // 16
+            Console.WriteLine(Math.Sqrt(16)); // 4
+            Console.WriteLine(Math.Abs(-100)); // 100
+            Console.WriteLine(Math.Round(3.7)); // 4
+            Console.WriteLine(Math.Floor(3.7)); // 3
+            Console.WriteLine(Math.Ceiling(3.3)); // 4
+            Console.WriteLine(Math.Max('a', 'b')); // 98 <--> b
+            Console.WriteLine(Math.Min('a', 'b')); // 97 <--> a
+
+            // Random numbers
+            Random ran = new Random();
+            Console.WriteLine(ran.Next(1, 7));
+            Console.WriteLine(ran.NextDouble()); // 0<= x <1
+
+
+            // Exo
+            Console.WriteLine("Enter side A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine("The hypotenus is: " + c);
+
+            // ---------------------------------- String methods ----------------------------------
+            string my_full_name = "Mourad ELCADI";
+            string my_phone_number = "123-456-7890";
+            string my_username = "modex";
+
+            Console.WriteLine(my_full_name + " --> " + my_full_name.ToUpper());
+            Console.WriteLine(my_full_name + " --> " + my_full_name.ToLower());
+            Console.WriteLine(my_phone_number + " --> " + my_phone_number.Replace('-', '/'));
+            Console.WriteLine(my_username + " --> " + my_username.Insert(0, "@"));
+            Console.WriteLine(my_username + " has length of " + my_username.Length);
+
+            string my_first_name = my_full_name.Substring(0, 6);
+            string my_last_name = my_full_name.Substring(7, 6);
+
+            Console.WriteLine("my first name " + my_first_name);
+            Console.WriteLine("my last name " + my_last_name);
+
+            // ---------------------------------- If statements ----------------------------------
+            Random my_ran = new Random();
+            
+            if(my_ran.NextDouble() * 10 > 5) 
+            {
+                Console.WriteLine("You got a number higher than 5");
+            }
+            else
+            {
+                Console.WriteLine("You got a number less or equale to 5");
+            }
+
+            // ---------------------------------- Rock-Paper-Scissors ----------------------------------
+
+            Random randy = new Random();
+            bool playAgain = true;
+            string player = "";
+            string computer = "";
+            string answer = "";
+
+            while(playAgain) 
+            {
+                while(player != "ROCK" && player != "PAPER" && player != "SCISSORS")
+                {
+                    Console.WriteLine("Enter Rock, Paper, or Scissors: ");
+                    player = Console.ReadLine()!;
+                    player = player.ToUpper();
+                }
+
+                switch (randy.Next(1, 4))
+                {
+                    case 1:
+                        computer = "ROCK";
+                        break;
+                    case 2:
+                        computer = "PAPER";
+                        break;
+                    default:
+                        computer = "SCISSORS"; // default is 3
+                        break;
+                }
+
+                Console.WriteLine("Player: " + player);
+                Console.WriteLine("Computer: " + computer);
+
+                switch (player)
+                {
+                    case "ROCK":
+                        if(computer == "ROCK") { Console.WriteLine("It's a draw!"); }
+                        else if(computer == "PAPER") { Console.WriteLine("You lose!"); }
+                        else { Console.WriteLine("You win!"); }
+                        break;
+                    case "PAPER":
+                        if(computer == "ROCK") { Console.WriteLine("You win!"); }
+                        else if(computer == "PAPER") { Console.WriteLine("It's a draw!"); }
+                        else { Console.WriteLine("You lose!"); }
+                        break;
+                    case "SCISSORS":
+                        if(computer == "ROCK") { Console.WriteLine("You lose!"); }
+                        else if(computer == "PAPER") { Console.WriteLine("It's a win!"); }
+                        else { Console.WriteLine("It's a draw!"); }
+                        break;
+                }
+
+                Console.Write("Would you like to play again ? (Y for yes): ");
+                answer = Console.ReadLine()!.ToUpper();
+
+                if(answer == "Y")  { player = ""; }
+                else playAgain = false;
+            }
+
+            Console.WriteLine("Thanks for playing!");
+
+            */
+
+            // ---------------------------------- Rock-Paper-Scissors ----------------------------------
         }
     }
 }
